e6fc46c8261a99c6d3326889b18ce59d3d8c26d8
econilod <117524160+econilod@users.noreply.github.com>
2022-11-15 23:25:05 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,21 @@
-
 var cache_name = "tomoriru-setsuna"
 
-var urlsToChache = [
+var cache_list = [
   "index.thml",
-  "icon-192x192.png",
-  "icon-256x256.png",
-  "icon-384x384.png",
-  "icon-512x512.png",
-  "manifest.json",
-  "sw.js"
+  "/icon-192x192.png",
+  "/icon-256x256.png",
+  "/icon-384x384.png",
+  "/icon-512x512.png",
+  "/manifest.json"
 ]
 
+self.addEventListener('install',function(e) {
+  e.waitUntil(
+    caches.open('chache_v1').then(function(cache) {
+      return cache.addAll(cache_list)
+    })
+  )
+})
+
 self.addEventListener('fetch', function(e) {
-  })
+})
